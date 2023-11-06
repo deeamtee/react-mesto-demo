064550b49060207b71e7c2991dc6395e2cb68cad
Эльдар <ethakohov@yandex.ru>
2023-11-06 10:06:59 +0300

    ПР-12

--- src/index.js
+++ src/index.js
@@ -17,9 +17,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-app.use(express.static(__dirname));
-
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
